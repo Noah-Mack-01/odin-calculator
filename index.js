fc1071b3edfd7dd7e97382ec3da0c49f5ea3502e
Noah-Mack-01 <noahnewmanmack@gmail.com>
2022-11-17 13:51:03 -0500

    Make key-presses responsive to text box
    Keys now mutate the input display of the
    calculator. I have to create a backspace
    function to delete elements of it, as well
    as a case for operators to erase the current
    displayed text, and more.

--- index.js
+++ index.js
@@ -4,6 +4,42 @@ const total = [...nums, ...operators];
 console.log(total);
 
 
+// we have two elements, really.
+// the number input, which inputs a number that will be added to our second
+// element, the operand chain, which is a series of functions to execute 
+// in the correct mathematical fashion.
+
+// lets work on making an operand first.
+// and operand is either a number, or an operator and two operands.
+// an operator is one of a few potential characters: '+','-','/','*'.
+// We will add exponents, parenthesis later. 
+
+// examples: Operand (+, 2 2) equates to four.
+let past_inp = 0;
+let cur_inp = "";
+
+function refresh() {
+    document.getElementById("input").innerHTML=cur_inp;
+}
+
+// lets define solve(operand):
+function solve(operand) {
+    if ("atomic" in operand) return operand.atomic;
+    switch (operand.operator) {
+        case '+':
+            return (solve(operand.one) + solve(operand.two));
+        case '-':
+            return (solve(operand.one) - solve(operand.two));
+        case '/':
+            return (solve(operand.one) / solve(operand.two));
+        case '*':
+            return (solve(operand.one) * solve(operand.two));
+        default:
+            break;
+    }
+    return "Error! Invalid Operator."
+}
+
 total.forEach(el => {
     el.addEventListener("mousedown", ()=>{
         el.classList.add("pressed");
@@ -27,6 +63,12 @@ window.addEventListener("keyup", (event) => {
     let key = document.querySelector(`div[data-key="${event.key}"]`);
     if (key == null || !key.classList.contains("pressed")) return;
     else key.classList.remove("pressed");
+
+    if (key.classList.contains("num")) {
+        cur_inp = cur_inp + event.key;
+    }
+
+    refresh();
 });
 
 
